36217473c4f1d2cef09477d4d9635bbb31024eab
yenohp <devinanderson286@gmail.com>
2022-04-24 19:38:40 -0700

    Created sign change functionality Created decimal functionality Fixed decimal disabled style

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const displayText = document.querySelector('h1');
 
 
 let arr = [];
+let value = 0;
 for (let button of buttons) {
     button.addEventListener('click', (e) => {
         let current = e.target;
@@ -10,12 +11,18 @@ for (let button of buttons) {
         if (current.className == 'numbers') {
             update(current);
             // Dont add zeros if the first number is zero
-            // If the decimal is clicked
-            // If the +/- is clicked
             // If the % is clicked
         }
+        // If the decimal is clicked
+        if (current.id == 'decimal') {
+            current.setAttribute('disabled', '');
+        }
+        // If the +/- is clicked
+        else if (current.id == 'sign') {
+            changeSign();
+        }
         // Clear display and array
-        if (current.id == 'clear') {
+        else if (current.id == 'clear') {
             empty(arr);
         }
         // Activate an operator
@@ -29,17 +36,33 @@ for (let button of buttons) {
 const activate = function (button) {
 
 }
+// Change the sign +/-
+const changeSign = function () {
+    if (value < 0) {
+        arr.splice(0, 1);
+        displayText.innerText = arr.join('');
+        return;
+    }
+    arr.splice(0, 0, '-');
+    value *= -1;
+    displayText.innerText = arr.join('');
+
+}
 const update = function (button) {
     arr.push(button.innerText);
     displayText.innerText = arr.join('');
+    value = arr.join('');
     console.log(arr);
 }
 const empty = function (arr) {
+    console.log('EMPTYING');
     if (arr.length == 0) return;
     let length = arr.length;
     for (let i = 0; i < length; i++) arr.pop();
     displayText.innerText = '0';
+    value = 0;
 }
+
 // Create a new function 'operate' that takes an operator and 2 numbers and then calls one of the operator functions on the numbers
 const operate = function (operator, x, y) {
     if (operator == '+') {
